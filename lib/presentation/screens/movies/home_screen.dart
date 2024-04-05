c519b1e0cf17ed944bcdd17eb10c847478454810
racmart <racmart00@gmail.com>
2024-04-05 15:30:57 -0400

    S13 - 004 Custom AppBar

--- lib/presentation/screens/movies/home_screen.dart
+++ lib/presentation/screens/movies/home_screen.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 
+import 'package:cinemapedia/presentation/widgets/widgets.dart';
 import 'package:cinemapedia/presentation/providers/movies/movies_providers.dart';
 
 class HomeScreen extends StatelessWidget {
@@ -37,14 +38,22 @@ class _HomeViewState extends ConsumerState<_HomeView> {
 
     if ( nowPlayingMovies.isEmpty ) return const Center(child: CircularProgressIndicator());
 
-    return ListView.builder(
-      itemCount: nowPlayingMovies.length,
-      itemBuilder: (context, index) {
-        final movie = nowPlayingMovies[index];
-        return ListTile(
-          title: Text( movie.title ),
-        );
-      },
+    return Column(
+      children: [
+        const CustomAppbar(),
+
+        Expanded(
+          child: ListView.builder(
+            itemCount: nowPlayingMovies.length,
+            itemBuilder: (context, index) {
+              final movie = nowPlayingMovies[index];
+              return ListTile(
+                title: Text( movie.title ),
+              );
+            },
+          ),
+        )
+      ],
     );
   }
 }
