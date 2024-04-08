f829122d899b7c0d6477546d5dccc44e71ebf49e
racmart <racmart00@gmail.com>
2024-04-08 13:44:59 -0400

    S13 - 008 Movies Slideshow Provider

--- lib/presentation/screens/movies/home_screen.dart
+++ lib/presentation/screens/movies/home_screen.dart
@@ -2,7 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 
 import 'package:cinemapedia/presentation/widgets/widgets.dart';
-import 'package:cinemapedia/presentation/providers/movies/movies_providers.dart';
+import 'package:cinemapedia/presentation/providers/providers.dart';
 
 class HomeScreen extends StatelessWidget {
   static const name = 'home-screen';
@@ -34,15 +34,16 @@ class _HomeViewState extends ConsumerState<_HomeView> {
 
   @override
   Widget build(BuildContext context) {
-    final nowPlayingMovies = ref.watch( nowPlayingMoviesProvider );
+    // final nowPlayingMovies = ref.watch( nowPlayingMoviesProvider );
+    final slideShowMovies = ref.watch( moviesSlideShowProvider );
 
-    if ( nowPlayingMovies.isEmpty ) return const Center(child: CircularProgressIndicator());
+    // if ( slideShowMovies.isEmpty ) return const Center(child: CircularProgressIndicator());
 
     return Column(
       children: [
         const CustomAppbar(),
 
-        MoviesSlideshow( movies: nowPlayingMovies ),
+        MoviesSlideshow( movies: slideShowMovies ),
       ],
     );
   }
