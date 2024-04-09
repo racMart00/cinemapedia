b6e6b73e8f99ed3739be34b6d792b1b0b5aa0a94
racmart <racmart00@gmail.com>
2024-04-09 15:30:05 -0400

    S14 - 006 MovieDB to Entity

--- lib/infrastructure/mappers/movie_mapper.dart
+++ lib/infrastructure/mappers/movie_mapper.dart
@@ -1,4 +1,5 @@
 import 'package:cinemapedia/domain/entities/movie.dart';
+import 'package:cinemapedia/infrastructure/models/moviedb/movie_details.dart';
 import 'package:cinemapedia/infrastructure/models/moviedb/movie_moviedb.dart';
 
 class MovieMapper {
@@ -16,7 +17,28 @@ class MovieMapper {
     popularity: moviedb.popularity,
     posterPath: (moviedb.posterPath != '')
      ? 'https://image.tmdb.org/t/p/w500${ moviedb.posterPath }'
-     : 'no-poster',
+     : 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSSx1NtyYxTFeZyMx6lJqzawh0l4jCcWFDDYx9JQZxnvJ4pfyIdqWfRq3NkMoAntxV7rGM&usqp=CAU',
+    releaseDate: moviedb.releaseDate,
+    title: moviedb.title,
+    video: moviedb.video,
+    voteAverage: moviedb.voteAverage,
+    voteCount: moviedb.voteCount
+  );
+
+  static Movie movieDetailsToEntity( MovieDetails moviedb ) => Movie(
+    adult: moviedb.adult,
+    backdropPath: (moviedb.backdropPath != '')
+     ? 'https://image.tmdb.org/t/p/w500${ moviedb.backdropPath }'
+     : 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSSx1NtyYxTFeZyMx6lJqzawh0l4jCcWFDDYx9JQZxnvJ4pfyIdqWfRq3NkMoAntxV7rGM&usqp=CAU',
+    genreIds: moviedb.genres.map((e) => e.name).toList(),
+    id: moviedb.id,
+    originalLanguage: moviedb.originalLanguage,
+    originalTitle: moviedb.originalTitle,
+    overview: moviedb.overview,
+    popularity: moviedb.popularity,
+    posterPath: (moviedb.posterPath != '')
+     ? 'https://image.tmdb.org/t/p/w500${ moviedb.posterPath }'
+     : 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSSx1NtyYxTFeZyMx6lJqzawh0l4jCcWFDDYx9JQZxnvJ4pfyIdqWfRq3NkMoAntxV7rGM&usqp=CAU',
     releaseDate: moviedb.releaseDate,
     title: moviedb.title,
     video: moviedb.video,
