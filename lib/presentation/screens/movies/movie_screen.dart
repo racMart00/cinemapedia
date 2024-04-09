b71aa347886b35a4a1c16ae90b6453d06cc2b6da
racmart <racmart00@gmail.com>
2024-04-09 16:18:16 -0400

    S14 - 008 Realizar la peticion HTTP y probar cache

--- lib/presentation/screens/movies/movie_screen.dart
+++ lib/presentation/screens/movies/movie_screen.dart
@@ -1,6 +1,11 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
 
-class MovieScreen extends StatefulWidget {
+import 'package:cinemapedia/presentation/providers/movies/movie_info_provider.dart';
+
+import 'package:cinemapedia/domain/entities/movie.dart';
+
+class MovieScreen extends ConsumerStatefulWidget {
   static const name = 'movie-screen';
 
   final String movieId;
@@ -11,18 +16,25 @@ class MovieScreen extends StatefulWidget {
   });
 
   @override
-  State<MovieScreen> createState() => _MovieScreenState();
+  MovieScreenState createState() => MovieScreenState();
 }
 
-class _MovieScreenState extends State<MovieScreen> {
+class MovieScreenState extends ConsumerState<MovieScreen> {
   @override
   void initState() {
     super.initState();
-    
+
+    ref.read( movieInfoProvider.notifier ).loadMovie( widget.movieId );
   }
 
   @override
   Widget build(BuildContext context) {
+    final Movie? movie = ref.watch( movieInfoProvider )[ widget.movieId ];
+
+    if(movie == null){
+      return const Scaffold(body: Center( child: CircularProgressIndicator( strokeWidth: 2 ) ));
+    }
+
     return Scaffold(
       appBar: AppBar(
         title: Text('Movie ID: ${widget.movieId}'),
