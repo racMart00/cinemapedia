985ffb58311268bc9a9d73415c6fb86d524c9a04
racmart <racmart00@gmail.com>
2024-04-11 18:15:47 -0400

    S14 - 015 Mostrar los actores de la pelicula

--- lib/presentation/screens/movies/movie_screen.dart
+++ lib/presentation/screens/movies/movie_screen.dart
@@ -129,11 +129,76 @@ class _MovieDetails extends StatelessWidget {
           ),
         ),
 
-        // TODO: Mostrar actores aqui
+        _ActorsByMovie( movieId: movie.id.toString() ),
 
         const SizedBox( height: 20 )
 
       ],
+    );
+  }
+}
+
+class _ActorsByMovie extends ConsumerWidget {
+  final String movieId;
+
+  const _ActorsByMovie({
+    required this.movieId
+  });
+
+  @override
+  Widget build(BuildContext context, ref) {
+    final actorsByMovie = ref.watch( actorsByMovieProvider );
+
+    if (actorsByMovie[movieId] == null) {
+      return const CircularProgressIndicator();
+    }
+
+    final actors = actorsByMovie[ movieId ]!;
+
+    return SizedBox(
+      height: 300,
+      child: ListView.builder(
+        scrollDirection: Axis.horizontal,
+        itemBuilder: (context, index) {
+          final actor = actors[index];
+
+          return Container(
+            padding: const EdgeInsets.all(8),
+            width: 135,
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+
+                //* Actor Photo
+                ClipRRect(
+                  borderRadius: BorderRadius.circular(20),
+                  child: Image.network(
+                    actor.profilePath,
+                    height: 180,
+                    width: 135,
+                    fit: BoxFit.cover,
+                  ),
+                ),
+
+                //* Separador
+                const SizedBox( height: 5 ),
+
+                //* Nombre
+                Text( actor.name, maxLines: 2 ),
+                Text(
+                  actor.character ?? '',
+                  maxLines: 2,
+                  style: const TextStyle(
+                    fontWeight: FontWeight.bold,
+                    overflow: TextOverflow.ellipsis
+                  ),
+                )
+
+              ],
+            ),
+          );
+        },
+      ),
     );
   }
 }
@@ -156,11 +221,11 @@ class _CustomSliverAppBar extends StatelessWidget {
       foregroundColor: Colors.white,
       flexibleSpace: FlexibleSpaceBar(
         titlePadding: const EdgeInsets.symmetric(horizontal: 10, vertical: 5),
-        title: Text(
-          movie.title,
-          style: const TextStyle( fontSize: 20 ),
-          textAlign: TextAlign.start,
-        ),
+        // title: Text(
+        //   movie.title,
+        //   style: const TextStyle( fontSize: 20 ),
+        //   textAlign: TextAlign.start,
+        // ),
         background: Stack(
           children: [
 
