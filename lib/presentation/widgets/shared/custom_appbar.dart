3845dd5335520d9068993d66ad221ddcd9ead3ab
racmart <racmart00@gmail.com>
2024-04-12 17:59:13 -0400

    S15 - 014 Search Movies Providers

--- lib/presentation/widgets/shared/custom_appbar.dart
+++ lib/presentation/widgets/shared/custom_appbar.dart
@@ -1,3 +1,4 @@
+import 'package:cinemapedia/presentation/providers/providers.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 
@@ -34,11 +35,16 @@ class CustomAppbar extends ConsumerWidget {
                 onPressed: () {
 
                   final movieRepository = ref.read( movieRepositoryProvider );
+                  final searchQuery = ref.read( searchQueryProvider );
 
                   showSearch<Movie?>(
+                    query: searchQuery,
                     context: context,
                     delegate: SearchMovieDelegate(
-                      searchMovies: movieRepository.searchMovies,
+                      searchMovies: ( query ) {
+                        ref.read( searchQueryProvider.notifier ).update(( state ) => query );
+                        return movieRepository.searchMovies( query );
+                      },
                     )
                   ).then((movie){
                     if (movie == null) return;
