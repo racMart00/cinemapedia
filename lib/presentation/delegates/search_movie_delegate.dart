314f8b98fecd9b3b4751ed06687b687fd6d93f09
racmart <racmart00@gmail.com>
2024-04-12 17:26:30 -0400

    S15 - 013 Debounce Movies - Stream

--- lib/presentation/delegates/search_movie_delegate.dart
+++ lib/presentation/delegates/search_movie_delegate.dart
@@ -17,11 +17,21 @@ class SearchMovieDelegate extends SearchDelegate<Movie?> {
     required this.searchMovies,
   });
 
+  void clearStreams() {
+    debouncedMovies.close();
+  }
+
   void _onQueryChanged( String query ){
     if ( _debounceTimer?.isActive ?? false ) _debounceTimer!.cancel();
 
-    _debounceTimer = Timer(const Duration( milliseconds: 500 ), () {
-      //TODO: buscar peliculas y emitir al stream
+    _debounceTimer = Timer(const Duration( milliseconds: 500 ), () async {
+      if ( query.isEmpty ) {
+        debouncedMovies.add([]);
+        return;
+      }
+
+      final movies = await searchMovies( query );
+      debouncedMovies.add( movies );
     });
   }
 
@@ -46,8 +56,11 @@ class SearchMovieDelegate extends SearchDelegate<Movie?> {
   @override
   Widget? buildLeading(BuildContext context) {
     return IconButton(
-      onPressed: () => close(context, null),
-      icon: const Icon( Icons.arrow_back_ios_new_rounded )
+      onPressed: () {
+        clearStreams();
+        close(context, null);
+      },
+      icon: const Icon( Icons.arrow_back_ios_new_rounded ),
     );
   }
 
@@ -70,7 +83,10 @@ class SearchMovieDelegate extends SearchDelegate<Movie?> {
           itemCount: movies.length,
           itemBuilder: (context, index) => _MovieItem(
             movie: movies[index],
-            onMovieSelected: close,
+            onMovieSelected: (context, movie) {
+              clearStreams();
+              close(context, movie);
+            },
           ),
         );
       },
