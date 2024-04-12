a7419a91ab687c630227457b0232409636daf39b
racmart <racmart00@gmail.com>
2024-04-12 14:02:28 -0400

    S15 - Leading y Actions

--- lib/presentation/delegates/search_movie_delegate.dart
+++ lib/presentation/delegates/search_movie_delegate.dart
@@ -1,21 +1,34 @@
 import 'package:flutter/material.dart';
+import 'package:animate_do/animate_do.dart';
 
-class SearchMovieDelegate extends SearchDelegate {
+import 'package:cinemapedia/domain/entities/movie.dart';
+
+class SearchMovieDelegate extends SearchDelegate<Movie?> {
   @override
   String? get searchFieldLabel => 'Buscar pelicula';
 
   @override
   List<Widget>? buildActions(BuildContext context) {
     return [
-
-      const Text('buildActions')
+      
+      FadeIn(
+        animate: query.isNotEmpty,
+        child: IconButton(
+          onPressed: () => query = '',
+          icon: const Icon( Icons.clear ),
+        ),
+      ),
+      
 
     ];
   }
 
   @override
   Widget? buildLeading(BuildContext context) {
-    return const Text('buildLeading');
+    return IconButton(
+      onPressed: () => close(context, null),
+      icon: const Icon( Icons.arrow_back_ios_new_rounded )
+    );
   }
 
   @override
