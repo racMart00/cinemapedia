3b8925176aa319254dc88a8629aadabef97c0e37
racmart <racmart00@gmail.com>
2024-04-15 11:08:58 -0400

    S16 - 005 ShellRoute - GoRouter

--- lib/config/router/app_router.dart
+++ lib/config/router/app_router.dart
@@ -8,26 +8,63 @@ final appRouter = GoRouter(
   initialLocation: '/',
   routes: [
 
-    GoRoute(
-      path: '/',
-      name: HomeScreen.name,
-      builder: (context, state) => const HomeScreen(
-        childView: HomeView(),
-      ),
+    ShellRoute(
+      builder: (context, state, child) {
+        return HomeScreen(childView: child);
+      },
       routes: [
 
         GoRoute(
-          path: 'movie/:id',
-          name: MovieScreen.name,
+          path: '/',
           builder: (context, state) {
-            final movieId = state.pathParameters['id'] ?? 'no-id';
+            return const HomeView();
+          },
+          routes: [
 
-            return MovieScreen(movieId: movieId);
+            GoRoute(
+              path: 'movie/:id',
+              name: MovieScreen.name,
+              builder: (context, state) {
+                final movieId = state.pathParameters['id'] ?? 'no-id';
+
+                return MovieScreen(movieId: movieId);
+              },
+            ),
+
+          ]
+        ),
+
+        GoRoute(
+          path: '/favorites',
+          builder: (context, state) {
+            return const FavoritesView();
           },
         ),
-        
-      ]
+
+      ],
     ),
+
+    //* Rutas padre/hijo
+    // GoRoute(
+    //   path: '/',
+    //   name: HomeScreen.name,
+    //   builder: (context, state) => const HomeScreen(
+    //     childView: HomeView(),
+    //   ),
+    //   routes: [
+
+    //     GoRoute(
+    //       path: 'movie/:id',
+    //       name: MovieScreen.name,
+    //       builder: (context, state) {
+    //         final movieId = state.pathParameters['id'] ?? 'no-id';
+
+    //         return MovieScreen(movieId: movieId);
+    //       },
+    //     ),
+        
+    //   ]
+    // ),
 
   ]
 );
